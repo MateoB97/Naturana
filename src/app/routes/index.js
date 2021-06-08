70cceccd29bb42e25f5d65cec4fb6b1dcbd2a48b
neiver97 <neiver.tapia@udea.edu.co>
2021-06-08 00:34:41 -0500

    add vistas al proyecto

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -5,40 +5,170 @@ const connection = require('../../config/db');
 
 module.exports = app =>{
 
+   //Atributes
+   let flagLogin= false;
+   let globalEmail;
    
     //Routes and connection  with views and database
 
     //gets
-   app.get('/login' , (req , res)=>{
-       res.render('../views/main/ventanas/login/login.ejs')
+   app.get('/' , (req , res)=>{
+       res.render('../views/main/ventanas/login/login.ejs');
    });
 
    app.get('/insumo' , (req , res)=>{
-      res.render('../views/main/ventanas/insumo/insumo.ejs')
+      let cont=0;
+      if (flagLogin=== false) {
+         res.render('../views/main/ventanas/login/login.ejs');
+      }else if(cont===0){
+         connection.query("SELECT firstName, lastName FROM insumo WHERE rol='ejemplo' AND user = ?",globalEmail,(err,result)=>{
+            try {
+               res.render('../views/main/ventanas/insumo/insumo.ejs',{
+                  name:result[0].firstName,
+                  lastName:result[0].lastName
+               });
+            } catch (error) {
+               console.error(`Error del try ${error}`);
+               console.error(`Error de la consulta ${err}`);
+               flagLogin=false;
+               globalEmail='';
+               res.redirect('/');
+            }
+         });
+         
+      }else if(cont!==0){
+         flagLogin=false;
+         globalEmail='';
+         res.redirect('/');
+      }
+      
    });
 
    app.get('/pedido' , (req , res)=>{
-      res.render('../views/main/ventanas/pedido/pedido.ejs')
+      let cont=0;
+      if (flagLogin=== false) {
+         res.render('../views/main/ventanas/login/login.ejs');
+      }else if(cont===0){
+         connection.query("SELECT firstName, lastName FROM users WHERE rol='Recepcionista' AND user= ?",globalEmail,(err, result)=>{
+            try {
+               res.render('../views/main/ventanas/pedido/pedido.ejs',{
+                  name:result[0].firstName,
+                  lastName: result[0].lastName
+               });
+            } catch (error) {
+               console.error(`Error del try ${error}`);
+               console.error(`Error del query ${err}`);
+               flagLogin=false;
+               globalEmail='';
+               res.redirect('/');
+            }
+         });
+         
+      }else if(cont!==0){
+         flagLogin=false;
+         globalEmail='';
+         res.redirect('/');
+      }
+
    });
+   
    app.get('/producto' , (req , res)=>{
-      res.render('../views/main/ventanas/producto/producto.ejs')
+
+      if (flagLogin=== false) {
+         res.render('../views/main/ventanas/login/login.ejs');
+      }else if(cont===0){
+         connection.query("SELECT firstName, lastName FROM users WHERE rol='ejemplo' AND user= ?",globalEmail,(err, result)=>{
+            try {
+               res.render('../views/main/ventanas/producto/producto.ejs',{
+                  name:result[0].firstName,
+                  lastName: result[0].lastName
+               });
+            } catch (error) {
+               console.error(`Error del try ${error}`);
+               console.error(`Error del query ${err}`);
+               flagLogin=false;
+               globalEmail='';
+               res.redirect('/');
+            }
+         });
+         
+      }else if(cont!==0){
+         flagLogin=false;
+         globalEmail='';
+         res.redirect('/');
+      }
    });
 
    app.get('/usuario' , (req , res)=>{
-      res.render('../views/main/ventanas/usuario/usuario.ejs')
+      let cont=0;
+      if (flagLogin=== false) {
+         res.render('../views/main/ventanas/login/login.ejs');
+      }else if(cont===0){
+         connection.query("SELECT firstName, lastName, rol FROM users WHERE rol='Administrador' AND user =?",globalEmail,(err, result)=>{
+            try {
+               res.render('../views/main/ventanas/usuario/usuario.ejs',{
+                  name:result[0].firstName,
+                  lastName: result[0].lastName
+               });
+            } catch (error) {
+               console.error(`Error del try ${error}`);
+               console.error(`Error de la consulta ${err}`);
+               flagLogin=false;
+               globalEmail='';
+               res.redirect('/');
+               
+            }
+         });
+         
+      }else if(cont!==0){
+         res.redirect('/');
+         globalEmail='';
+      }
    });
 
    app.get('/main' , (req , res)=>{
-      res.render('../views/main/main.ejs')
+      
+      let cont=0;
+
+      if (flagLogin=== false) {
+         res.render('../views/main/ventanas/login/login.ejs');
+      }else if(cont===0){
+         connection.query("SELECT firstName, lastName FROM users WHERE rol = 'Administrador' AND user= ?",globalEmail,(err,result)=>{
+            try {
+               res.render('../views/main/main.ejs',{
+                  name:result[0].firstName,
+                  lastName: result[0].lastName
+               });
+            } catch (error) {
+               console.error(` Error del catch ${error}`);
+               console.error(` Error del query ${err}`);
+               flagLogin=false;
+               globalEmail='';
+               res.redirect('/');
+            }
+            //Here
+            cont++;
+         });
+         console.log(cont);
+         //Ask to Jairo
+      }else if(cont!==0){
+         globalEmail='';
+         res.redirect('/');
+         
+      }
+      
    });
 
-   //posts
+   
 
+
+
+   //posts
     //Login for users 
     app.post('/auth' , async(req , res)=>{
-        const {email, password}  = req.body;
-        let flagLogin= false;
 
+        const {email, password}  = req.body;
+         globalEmail=email;  
          if (email && password) {
             connection.query('SELECT user, pass, rol FROM users WHERE user= ?', email, async(err,results)=>{
                try {
@@ -51,7 +181,7 @@ module.exports = app =>{
                            alertIcon: "warning",
                            showConfirmButton: true,
                            timer:false,
-                           ruta:'login'
+                           ruta:'/'
                        });
                      } catch (error) {
                         console.error(error);
@@ -125,5 +255,9 @@ module.exports = app =>{
          }
      });
 
+     app.post('/session-out' , (req , res)=>{
+      flagLogin=false;
+      res.render('../views/main/ventanas/login/login.ejs');
+   });
    
 }
